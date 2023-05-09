3d999b7dad5c8e71a628243cac05fdfce58e16ab
EtiamSani <etiam.sani@laposte.net>
2023-05-09 18:09:24 +0200

    Errorboundaries

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,30 @@
+// Utiliser Class component pour la gesiton des erreurs.
+
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Errorboundary extends Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary component caught an error", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h2>
+          There was an erreor with this listing.
+          <Link to="/">Click here to go back to the home page.</Link>
+        </h2>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default Errorboundary;
